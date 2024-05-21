fb5a2070c6b2594bc50ab45040aae34d7a5ca621
mcalderon <mcalderon>
2024-05-21 01:42:15 -0500

    se agrego todo

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'LA FE MRDA VERCEL';
+    return 'MATRIZ DE RIESGO';
   }
 }
